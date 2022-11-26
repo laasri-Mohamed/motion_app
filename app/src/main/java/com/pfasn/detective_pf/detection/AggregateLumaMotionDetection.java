c5f5cffad12be188ea7a51be382af6561a982624
laasri-Mohamed <laasrimed2001@gmail.com>
2022-11-26 18:24:03 +0100

    final version

--- app/src/main/java/com/pfasn/detective_pf/detection/AggregateLumaMotionDetection.java
+++ app/src/main/java/com/pfasn/detective_pf/detection/AggregateLumaMotionDetection.java
@@ -5,7 +5,14 @@ package com.pfasn.detective_pf.detection;
 
 public class AggregateLumaMotionDetection implements IMotionDetection {
 
-    
+    // private static final String TAG = "AggregateLumaMotionDetection";
+
+    // Specific settings
+    private static final int mLeniency = 10; // Difference of aggregate map of
+                                             // luma values
+    private static final int mDebugMode = 2; // State based debug
+    private static final int mXBoxes = 10; // State based debug
+    private static final int mYBoxes = 10; // State based debug
 
     private static int[] mPrevious = null;
     private static int mPreviousWidth;
